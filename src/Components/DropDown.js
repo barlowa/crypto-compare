90ef46408b102663814c406de7350dc845fa6a7e
alex <35688619+barlowa@users.noreply.github.com>
2020-07-12 15:54:47 +0100

    created drop down/select box component

---
+++ src/Components/DropDown.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SelectStyle = styled.select`
+	width: 200px;
+	height: 40px;
+	border-width: 0 0 1px 0;
+`;
+const DropDown = ({ onChange, value, children }) => {
+	return (
+		<SelectStyle onChange={onChange} value={value}>
+			{children}
+		</SelectStyle>
+	);
+};
+
+DropDown.propTypes = {
+	children: PropTypes.any,
+	onChange: PropTypes.func.isRequired,
+	value: PropTypes.oneOfType(PropTypes.string, PropTypes.number).isRequired,
+};
+
+export default DropDown;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as DataTable } from './DataTable';
 export { default as IconButton } from './IconButton';
 export { default as Loading } from './Loading';
+export { default as DropDown } from './DropDown';
